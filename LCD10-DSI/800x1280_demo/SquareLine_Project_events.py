539c7340518b8705c2ba130ee02d091c5e372281
Sukkin Pang <skp@skpang.co.uk>
2025-09-24 21:46:11 +0100

    Add files via upload

---
+++ LCD10-DSI/800x1280_demo/SquareLine_Project_events.py
@@ -0,0 +1,16 @@
+
+def brightness_inc(event_struct):
+    return
+
+
+def brightness_dec(event_struct):
+    return
+
+
+def stop_can(event_struct):
+    return
+
+
+def clear_frame(event_struct):
+    return
+
